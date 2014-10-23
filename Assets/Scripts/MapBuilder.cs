66b870ac3b1dccd1ad9ad69531d2df7efc239210
Gabriel Zanzotti <gabriel.zanzotti@gmail.com>
2014-10-23 02:12:10 -0300

    terrain

---
+++ Assets/Scripts/MapBuilder.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapBuilder : MonoBehaviour {
+
+	public GameObject block;
+	public GameObject block2;
+	// Use this for initialization
+	void Start () {
+		block.transform.position = new Vector3 (0f, 10f, 0f);
+		block2.transform.position = new Vector3 (-1000f, 10f, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
