b9eab8c20bae9b44eee83ed01ef602890b14978e
Kaz <inakilanusse@gmail.com>
2014-10-29 22:59:05 -0300

    Added Quit

---
+++ Assets/Quit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quit : MonoBehaviour {
+
+	public KeyCode escape;
+	// Update is called once per frame
+	void Update () {
+		Screen.showCursor = false;
+		if (Input.GetKey (escape)) {
+			Application.LoadLevel("menu");
+		}
+	}
+}
