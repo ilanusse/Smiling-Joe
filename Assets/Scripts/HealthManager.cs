e00222155d56c9c0f1aa8bec72443661f61431b8
Gabriel Zanzotti <gabriel.zanzotti@gmail.com>
2014-10-29 22:34:00 -0300

    [master] just some small tweeks.

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -6,8 +6,6 @@ public class HealthManager : MonoBehaviour {
 	private float healthTimer;
 	public float inSightHealthTime;
 	public VictoryManager victoryManager;
-	public TipManager tipManager;
-
 
 	// Use this for initialization
 	void Start () {
